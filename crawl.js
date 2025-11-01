a55b1c3bcd4d2a65792c0a4168f79cf026df484c
Namita Singh <ns.namitasingh15@gmail.com>
2025-11-01 19:39:22 +0530

    get urls from html functions and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,35 @@
+const {JSDOM} = require('jsdom')
+function getURLsfromHTML(htmlBody,baseURL)
+{
+    const urls= []
+    const dom= new JSDOM(htmlBody)
+    const linkElements= dom.window.document.querySelectorAll('a')
+    for(const linkElement of linkElements)
+    {
+        if(linkElement.href.slice(0,1) === '/')
+        {
+            try{
+            //relative
+            const urlObj = new URL(`${baseURL}${linkElement.href}`)
+            urls.push(urlObj.href)
+            }catch(err){
+                console.log(`error with relative url: ${err.message}`)
+            }
+        }
+        else
+        {
+            try{
+            //absolute
+            const urlObj = new URL(linkElement.href)
+            urls.push(urlObj.href)
+            }catch(err){
+                console.log(`error with absolute url: ${err.message}`)
+            }
+        }
+        
+    }
+    return urls
+}
 function normalizeURL(urlString)
 {
     const urlObj = new URL(urlString);
@@ -10,5 +42,6 @@ function normalizeURL(urlString)
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsfromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const {normalizeURL} = require('./crawl.js');
+const {normalizeURL,getURLsfromHTML} = require('./crawl.js');
 const {test,expect} = require('@jest/globals');
 
 test('normalizeURL strip protocol', ()=> {
@@ -28,5 +28,93 @@ test('normalizeURL strip http', ()=> {
     const input = 'http://namita.buchku.dev/path/'
     const actual = normalizeURL(input)
     const expected = 'namita.buchku.dev/path'
+    expect(actual).toEqual(expected)
+} );
+
+test('getURLsfromHTML', ()=> {
+    const inputHTMLBody =
+    `
+    <html>
+        <body>
+        <a href="https://namita.buchku.dev/">
+        Namita Blog
+        </a>
+        </body>
+    </html>
+    `
+    const inputBaseURL = "http://namita.buchku.dev"
+    const actual = getURLsfromHTML(inputHTMLBody,inputBaseURL)
+    const expected = ["https://namita.buchku.dev/"]
+    expect(actual).toEqual(expected)
+} );
+
+test('getURLsfromHTML absolute', ()=> {
+    const inputHTMLBody =
+    `
+    <html>
+        <body>
+        <a href="https://namita.buchku.dev/path/">
+        Namita Blog
+        </a>
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://namita.buchku.dev/path/"
+    const actual = getURLsfromHTML(inputHTMLBody,inputBaseURL)
+    const expected = ["https://namita.buchku.dev/path/"]
+    expect(actual).toEqual(expected)
+} );
+
+test('getURLsfromHTML relative', ()=> {
+    const inputHTMLBody =
+    `
+    <html>
+        <body>
+        <a href="/path/">
+        Namita Blog
+        </a>
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://namita.buchku.dev"
+    const actual = getURLsfromHTML(inputHTMLBody,inputBaseURL)
+    const expected = ["https://namita.buchku.dev/path/"]
+    expect(actual).toEqual(expected)
+} );
+
+test('getURLsfromHTML both', ()=> {
+    const inputHTMLBody =
+    `
+    <html>
+        <body>
+        <a href="/path1/">
+        Namita Blog path 1
+        </a>
+        <a href="https://namita.buchku.dev/path2/">
+        Namita Blog
+        </a>
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://namita.buchku.dev"
+    const actual = getURLsfromHTML(inputHTMLBody,inputBaseURL)
+    const expected = ["https://namita.buchku.dev/path1/","https://namita.buchku.dev/path2/"]
+    expect(actual).toEqual(expected)
+} );
+
+test('getURLsfromHTML invalid', ()=> {
+    const inputHTMLBody =
+    `
+    <html>
+        <body>
+        <a href="invalid">
+        Invalid url
+        </a>
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://namita.buchku.dev"
+    const actual = getURLsfromHTML(inputHTMLBody,inputBaseURL)
+    const expected = []
     expect(actual).toEqual(expected)
 } );
